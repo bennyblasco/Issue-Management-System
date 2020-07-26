f0ad2029884fdd06b9b7e5a994e16ec4922ad94d
Bennyblasco <bennyblasco@gmail.com>
2020-07-26 17:30:40 +0100

    New files added

---
+++ IssueMS/Models/Issue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IssueMS.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Web Application")]
+        public string AppId { get; set; }
+        public string Title { get; set; }
+        public string Report { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Time { get; set; }
+        
+
+    }
+}
